5a255bef5cb6501e9a865452f3968509292eb4c4
Sean Hamm <seanhamm28@gmail.com>
2024-05-22 20:54:18 -0700

    adding router added

--- MUI/mui-demo/src/App.jsx
+++ MUI/mui-demo/src/App.jsx
@@ -13,11 +13,34 @@ function App() {
 
   return (
     <>
-      <VariantButtonGroup buttonOne="Worker 1" buttonTwo="Worker 2" buttonThree="Worker 3" />
+<div>
+<h1>***Creator notes for future feature releases***</h1>
+<p>Here are some examples of links to other pages</p>
+<ul><p></p></ul>
+<nav>
+  <ul>
+    <li>
+      <a href="profile">Profile page test</a><br></br>
+      <a href="profile">Phonebook page</a>
+    </li>
+  </ul>
+</nav>
+</div>
+
+
+      {/*<VariantButtonGroup buttonOne="Worker 1" buttonTwo="Worker 2" buttonThree="Worker 3" />*/}
       <InputForm onSubmit={handleSubmit} />
       <ServiceTeamTable data={formData} />
-    </>
-  )
+   
+
+
+ </>
+)
+
+
+
 }
+
+
 
 export default App
---
+++ MUI/mui-demo/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+      <div>
+        <h1>User Notes from the Main Page</h1>
+        <p>Please verify your projects against the service team phonebook</p>
+      </div>
+    );
+  };
+  
+  export default Profile;
+  
